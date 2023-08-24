91f614592d6cca6cc6fc6328b24d5d4678b14ce7
Andrew Katz <GamechallengerA@gmail.com>
2023-08-24 17:39:59 +0000

    first code of the year

--- WebDev3.py
+++ WebDev3.py
@@ -1,1 +1,9 @@
-print("hi")
+print("Welcome to the Space Travel Calculator")
+
+distance = float(input("Enter distance to the celestial object1")) 
+
+speed = float(input("type to speed of your space ship")) 
+
+time = distance / speed 
+
+print("it will take",  time, "hours to reach your spaceship ")
